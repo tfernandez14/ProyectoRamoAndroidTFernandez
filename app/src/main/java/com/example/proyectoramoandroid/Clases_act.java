ea92c14f74fe886c383ed3cac84dca779a6f9e61
Tomas Ignacio Fernández Álvarez <t.fernandez14@alumnos.santotomas.cl>
2021-11-18 18:18:26 -0300

    1313

--- app/src/main/java/com/example/proyectoramoandroid/Clases_act.java
+++ app/src/main/java/com/example/proyectoramoandroid/Clases_act.java
@@ -3,6 +3,7 @@ package com.example.proyectoramoandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -55,14 +56,74 @@ public class Clases_act extends AppCompatActivity
 
     public void mostrarClases(View view)
     {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"biofit", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
 
+        String codigo = code.getText().toString();
+
+        if (!codigo.isEmpty())
+        {
+            Cursor file = db.rawQuery("SELECT clase, intensidad FROM clases WHERE codigo="+codigo, null);
+            if (file.moveToFirst())
+            {
+                clas.setText(file.getString(0));
+                intensi.setText(file.getString(1));
+            }
+            else
+            {
+                Toast.makeText(getBaseContext(),"NO hay clase asociada.. ", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else
+        {
+            Toast.makeText(getBaseContext(),"El cogido  viene vacio.. ", Toast.LENGTH_SHORT).show();
+        }
     }
     public void eliminarClases(View view)
     {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"biofit", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+
+        String codigo = code.getText().toString();
+
+        if (!codigo.isEmpty())
+        {
+            db.delete("clases", "codigo="+codigo,null);
+            db.close();
+            Clean();
+            Toast.makeText(getBaseContext(),"Se a eleminado con exito.. "+codigo, Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(getBaseContext(),"El cogido no debe venir vacio.. ", Toast.LENGTH_SHORT).show();
+        }
 
     }
     public void actualizarClases(View view)
     {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"biofit", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+
+        String codigo = code.getText().toString();
+        String clase = clas.getText().toString();
+        String intensidad = intensi.getText().toString();
+
+        if(!codigo.isEmpty() && !clase.isEmpty() && !intensidad.isEmpty())
+        {
+            ContentValues cont = new ContentValues();
+            cont.put("clase", clase);
+            cont.put("intensidad", intensidad);
+
+            db.update("clases",cont,"codigo="+codigo,null);
+            db.close();
+            Clean();
+            Toast.makeText(getBaseContext(),"Has actualizado  una clase",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(getBaseContext(),"Hay campos vacios.. ", Toast.LENGTH_SHORT).show();
+        }
+
 
     }
     public  void Clean()
